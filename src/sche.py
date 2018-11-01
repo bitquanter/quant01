c80a9ab8cc5a2df8aae890e810d5935d1fa65bb4
732047249 <jiaolifeng@yahoo.com>
2018-11-02 07:25:06 +0800

    Update sche.py

--- src/sche.py
+++ src/sche.py
@@ -1,5 +1,9 @@
+# coding:utf-8
+
 import gevent
- 
+from gevent import monkey
+monkey.patch_all()
+
 from apscheduler.schedulers.blocking import BlockingScheduler
 import datetime
 
@@ -7,16 +11,20 @@ import datetime
 def aps_test():
     print (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 
+def aps_test1():
+    print ('test1')
 
 def sche():
 	scheduler = BlockingScheduler()
 	scheduler.add_job(func=aps_test, trigger='cron', second='*/2')
+	scheduler.add_job(func=aps_test1, trigger='cron', second='*/2')
 	scheduler.start()
  
 def bar():
-    print('.')
-    gevent.sleep(0)
-    print('#')
+	while 1:
+		print('.')
+		gevent.sleep(0)
+		print('#')
  
 gevent.joinall([
     gevent.spawn(sche),
